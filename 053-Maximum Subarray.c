36b69885503c522ec7f8b24d1a1736bd0931c3a8
Super Ryan <zjryane@gmail.com>
2015-05-19 23:58:30 +0800

    Update 053-Maximum Subarray.c

--- 053-Maximum Subarray.c
+++ 053-Maximum Subarray.c
@@ -2,7 +2,6 @@ int maxSubArray(int* nums, int numsSize) {
     int i;
     int sum = nums[0];
     int maxSum = nums[0];
-    
     for(i = 1; i < numsSize; i++){
         if(sum < 0){
             sum = 0;
