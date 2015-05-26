7ce6ed86a1e9c2360c329b06f3a6acb8a5b84210
Super Ryan <zjryane@gmail.com>
2015-05-26 13:11:06 +0800

    retry

--- 119-Pascal's Triangle II.cpp
+++ 119-Pascal's Triangle II.cpp
@@ -5,8 +5,9 @@ public:
         for(int i = 0;i <= rowIndex;i++)  
         {  
             int k = (int)level.size();  
-            for(int j = k - 1; j >= 1; j--)  
-                level[j] += level[j-1];  
+            for(int j = k - 1; j >= 1; j--) {
+                level[j] += level[j-1];
+            }
             level.push_back(1);  
         }  
         return level;          
